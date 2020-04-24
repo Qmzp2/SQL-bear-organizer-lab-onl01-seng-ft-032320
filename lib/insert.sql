8f7ffd7c48b983ba57f1a2d1c5218eebfd3a1b0c
Kevin Mathias <komathias1@gmail.com>
2020-04-24 23:03:19 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 9001, "male", "brown", "chill", 1);
