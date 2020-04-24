8deb9b50b73ecc9fbe0dbea0a216d2db3873686b
Kevin Mathias <komathias1@gmail.com>
2020-04-24 23:04:17 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,7 +2,7 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Cho
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 9001, "male", "brown", "chill", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 9001, "male", "brown", "chill", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 9001, "male", "brown", "chill", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Chocolate", 9001, "male", "brown", "chill", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 9001, "male", "brown", "chill", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Chocolate", 9001, "male", "brown", "chill", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Chocolate", 9001, "male", "brown", "chill", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Chocolate", 9001, "male", "brown", "chill", 1);
