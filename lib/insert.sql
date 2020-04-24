6d500eb9ec36cfc5fb122f3d0b6c31c3f707c531
Kevin Mathias <komathias1@gmail.com>
2020-04-24 23:04:02 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 9001, "male", "brown", "chill", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 9001, "male", "brown", "chill", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 9001, "male", "brown", "chill", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 9001, "male", "brown", "chill", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 9001, "male", "brown", "chill", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 9001, "male", "brown", "chill", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 9001, "male", "brown", "chill", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 9001, "male", "brown", "chill", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 9001, "male", "brown", "chill", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Chocolate", 9001, "male", "brown", "chill", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Chocolate", 9001, "male", "brown", "chill", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Chocolate", 9001, "male", "brown", "chill", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Chocolate", 9001, "male", "brown", "chill", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Chocolate", 9001, "male", "brown", "chill", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Chocolate", 9001, "male", "brown", "chill", 1);
